22f4d503c5ee75dd34d02b1114c181031d8204ed
Arend Hosman <info@webbb-online.com>
2018-01-17 11:38:20 +0100

    Adds PostList component code

---
+++ frontend/src/components/PostList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Post from './Post';
+import { fetchPosts } from '../actions'
+import { connect } from 'react-redux';
+
+class PostList extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    return(
+      <ul>
+        {this.props.postList.map(post => <li key={post.id}><Post post={post} /></li>)}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    postList: state.posts,
+  }
+}
+
+export default connect(mapStateToProps)(PostList)
