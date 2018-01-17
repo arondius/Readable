321be47ef0e49c178a38c7d76015591f0c4c156c
Arend Hosman <info@webbb-online.com>
2018-01-17 11:37:34 +0100

    Adds App component code

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PostList from './PostList';
+import PostSingle from './PostSingle';
+import '../App.css';
+import { Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={PostList} />
+        <Route path="/post/:id" component={PostSingle}/>
+      </div>
+    );
+  }
+}
+
+export default App;
