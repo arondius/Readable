755d7efa5bbbc4b9d8e0432b24d2b03e7dda73e9
Arend Hosman <info@webbb-online.com>
2018-01-18 15:07:04 +0100

    Groups actions together with their action creators

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,27 +1,19 @@
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const FETCH_POST = 'FETCH_POST';
-export const ADD_POST = 'ADD_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const UPDATE_POST = 'UPDATE_POST';
-
-export const FETCH_COMMENTS = 'FETCH_COMMENTS';
-export const ADD_COMMENT = 'ADD_COMMENT';
-export const DELETE_COMMENT = 'DELETE_COMMENT';
-export const UPDATE_COMMENT = 'UPDATE_COMMENT';
-
 export const UPP_VOTE = 'UPP_VOTE';
 export const DOWN_VOTE = 'DOWN_VOTE';
 
-// Posts action creators
+
+export const FETCH_POSTS = 'FETCH_POSTS';
 export const fetchPosts = () => ({
   type: FETCH_POSTS,
 })
 
+export const FETCH_POST = 'FETCH_POST';
 export const fetchPost = (id) => ({
   type: FETCH_POST,
   id
 })
 
+export const ADD_POST = 'ADD_POST';
 export const addPost = (id, title, body) => ({
   type: ADD_POST,
   id,
@@ -29,31 +21,37 @@ export const addPost = (id, title, body) => ({
   body
 })
 
+export const DELETE_POST = 'DELETE_POST';
 export const deletePost = (id) => ({
   type: DELETE_POST,
   id
 })
 
+export const UPDATE_POST = 'UPDATE_POST';
 export const updatePost = (id) => ({
   type: UPDATE_POST,
   id
 })
 
 // Comments action creators
+export const FETCH_COMMENTS = 'FETCH_COMMENTS';
 export const fetchComments = () => ({
   type: FETCH_COMMENTS,
 })
 
+export const ADD_COMMENT = 'ADD_COMMENT';
 export const addComment = (id) => ({
   type: ADD_COMMENT,
   id
 })
 
+export const DELETE_COMMENT = 'DELETE_COMMENT';
 export const deleteComment = (id) => ({
   type: DELETE_COMMENT,
   id
 })
 
+export const UPDATE_COMMENT = 'UPDATE_COMMENT';
 export const updateComment = (id) => ({
   type: UPDATE_COMMENT,
   id
