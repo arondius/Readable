588b81350794be62cfd5900d4935e25ef0e17f3e
Arend Hosman <info@webbb-online.com>
2018-01-26 14:32:47 +0100

    Render postform on edit / add post

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,22 +3,82 @@ import uuidGen from 'uuid';
 import { connect } from 'react-redux';
 import { addPost, deletePost, updatePost } from '../actions'
 import { Link } from 'react-router-dom'
+import PostForm from './PostForm'
 
-class Post extends Component {
-  onAddPost() {
-    const uuid = uuidGen();
-    this.props.dispatch(addPost(uuid, `TITEL-${uuid}`, `BODY-${uuid}`));
+class Post extends Component {  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      postForm: {
+        isOpen: false,
+        postEditing: null
+      }
+    };
   }
   
-  onDeletePost() {
-    this.props.dispatch(deletePost('4dd15ac9-0468-4d21-a8be-9adaf6d77c6a'));
+  handleDeletePost() {
+    this.props.dispatch(deletePost(this.props.post.id));
   }
   
-  onUpdatePost(id) {
-    this.props.dispatch(updatePost(id));
+  handleEditPost(post = null, e) {
+    e.preventDefault();
+    // the form is open
+    if(this.state.postForm.isOpen) {
+      // are we editing a post?
+      if(this.state.postForm.postEditing) { 
+        this.setState((state) => (
+          {
+            ...state,
+              postForm: {
+                ...state.postForm,
+                  postEditing: null
+              }
+          }
+        ));
+      }
+      // close form
+      this.setState((state) => (
+        {
+          ...state,
+            postForm: {
+              ...state.postForm,
+                isOpen: false
+            }
+        }
+      ));
+    }
+    // the form is closed 
+    else { 
+      // did we receive a post (I.E. do we edit an exisitng post?)
+      if(post) {
+        console.log('post',post)
+        this.setState((state) => (
+          {
+            ...state,
+            postForm: {
+              ...state.postForm,
+                postEditing: post
+            }
+          }
+        ));
+      }
+      // open form
+      this.setState(state => (
+        {
+          ...state,
+            postForm: {
+              ...state.postForm,
+                isOpen: true
+          }
+        }
+      ));
+    }
   }
-  
+
   render() {
+    // console.log('Post.js this.props', this.props);
+    console.log('Post.js this.state', this.state);
     return(
       <div>
         <h1><Link to={`/post/${this.props.post.id}`}>{this.props.post.title}</Link></h1>
@@ -26,8 +86,10 @@ class Post extends Component {
         <p>{this.props.post.author}</p>
         <p>{this.props.post.category}</p>
         <p>{this.props.post.voteScore}</p>
-        <button onClick={() => this.onEditPost()} />
-        <button onClick={() => this.onUpdatePost(this.props.post.id)}/>
+        <button onClick={(e) => this.handleEditPost(e)}>Add Post</button>
+        <button onClick={(e) => this.handleEditPost(this.props.post, e)}>Edit Post</button>
+        <button onClick={(e) => this.handleAddPost(e)}>Delete Post</button>
+        {this.state.postForm.isOpen ? <PostForm /> : null}
       </div>
     )
   }
---
+++ frontend/src/components/PostForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostForm extends Component {
+  render() {
+    return(
+      <div className="create-post__container">
+        <h2>Add a post</h2>
+        bla bla bla
+      </div>
+    )
+  }
+}
+
+export default PostForm;
