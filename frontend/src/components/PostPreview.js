aadc43fb3588e1264a745ce323ddccf71fa6d692
Arend Hosman <info@webbb-online.com>
2018-02-10 18:31:01 +0100

    Removes import

--- frontend/src/components/PostPreview.js
+++ frontend/src/components/PostPreview.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {togglePostForm} from '../actions'
 import { Link } from 'react-router-dom'
 import VoteWidget from './VoteWidget'
 
