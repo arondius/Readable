97efcf313429ad461deef4755d90346675414e4a
Arend Hosman <info@webbb-online.com>
2018-03-08 10:12:57 +0100

    Adds new default posts

--- api-server/posts.js
+++ api-server/posts.js
@@ -14,9 +14,9 @@ const defaultData = {
     deleted: false,
     commentCount: 2
   },
-  "6ni6ok3ym7mf1p33lnez": {
-    id: '6ni6ok3ym7mf1p33lnez',
-    timestamp: 1468479767190,
+  "6ni6ok3ym7mf1p33l1d6": {
+    id: '6ni6ok3ym7mf1p33l1d6',
+    timestamp: 1460479767190,
     title: 'Learn Redux in 10 minutes!',
     body: 'Just kidding. It takes more than 10 minutes to learn technology.',
     author: 'thingone',
@@ -24,7 +24,117 @@ const defaultData = {
     voteScore: -5,
     deleted: false,
     commentCount: 0
-  }
+  },
+  "6ni6ok3ym7mf1p33l8fo": {
+    id: '6ni6ok3ym7mf1p33l8fo',
+    timestamp: 1461479768190,
+    title: 'Learn Redux in 15 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33ln7gd": {
+    id: '6ni6ok3ym7mf1p33ln7gd',
+    timestamp: 1462479769190,
+    title: 'Learn Redux in 20 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lnud8": {
+    id: '6ni6ok3ym7mf1p33lnud8',
+    timestamp: 1463479777190,
+    title: 'Learn Redux in 30 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33l5th": {
+    id: '6ni6ok3ym7mf1p33l5th',
+    timestamp: 1464479787190,
+    title: 'Learn Redux in 40 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lk0c": {
+    id: '6ni6ok3ym7mf1p33lk0c',
+    timestamp: 1465479797190,
+    title: 'Learn Redux in 50 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lz9d": {
+    id: '6ni6ok3ym7mf1p33lz9d',
+    timestamp: 1466479807190,
+    title: 'Learn Redux in 60 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33l3m6": {
+    id: '6ni6ok3ym7mf1p33l3m6',
+    timestamp: 1467479907190,
+    title: 'Learn Redux in 70 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33l098": {
+    id: '6ni6ok3ym7mf1p33l098',
+    timestamp: 1468471007190,
+    title: 'Learn Redux in 80 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33l76t": {
+    id: '6ni6ok3ym7mf1p33l76t',
+    timestamp: 1469479101190,
+    title: 'Learn Redux in 90 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lv78": {
+    id: '6ni6ok3ym7mf1p33lv78',
+    timestamp: 1470479102190,
+    title: 'Learn Redux in 100 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
 }
 
 function getData (token) {
