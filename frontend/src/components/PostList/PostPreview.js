3f7f5b21a7500d97fac714bde413100523dd8b84
Arend Hosman <info@webbb-online.com>
2018-03-17 11:04:53 +0100

    Fixes post link on button

--- frontend/src/components/PostList/PostPreview.js
+++ frontend/src/components/PostList/PostPreview.js
@@ -10,7 +10,8 @@ class PostPreview extends Component {
   render() {
     
     const {id} = this.props.post
-    const postDate = new Date(this.props.post.timestamp);
+    const postDate = new Date(this.props.post.timestamp)
+    const postLink = `/${this.props.post.category}/${this.props.post.id}`
     const postValues = {
       postTitle: this.props.post.title,
       postBody: this.props.post.body,
@@ -21,7 +22,7 @@ class PostPreview extends Component {
         <h1>
           <Link 
             className="title-link"
-            to={`/${this.props.post.category}/${this.props.post.id}`}>{this.props.post.title}
+            to={postLink}>{this.props.post.title}
           </Link>
         </h1>
         <p>Author: {this.props.post.author}</p>
@@ -29,7 +30,7 @@ class PostPreview extends Component {
         <p>Posted on: {postDate.toDateString()}</p>
         <p># of Comments: {this.props.post.commentCount}</p>
         <VoteWidget id={this.props.post.id} type="posts" voteScore={this.props.post.voteScore} />
-        <Link className="btn btn--primary" to={`/post/${this.props.post.id}`}>View Post</Link>
+        <Link className="btn btn--primary" to={postLink}>View Post</Link>
         <button className="btn" onClick={() => this.props.toggleEditPostClick(id)}>Edit post</button>
         <button className="btn btn__" onClick={() => this.props.dispatch(deletePost(this.props.post.id))}>Delete post</button>
         {(this.props.postEditForm.open !== false && this.props.postEditForm.id === id)  ? 
