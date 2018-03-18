ce11c21dab16c3869951a9e9ba1ddfef794c86dd
Arend Hosman <info@webbb-online.com>
2018-03-18 17:14:09 +0100

    Changes to stateless component

--- frontend/src/components/VoteWidget.js
+++ frontend/src/components/VoteWidget.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { vote } from '../actions/voteActions'
-class VoteWidget extends Component {
 
-  render() {
-    return(
-      <div className="vote-widget__container">
-        <p className="vote-widget__votes">vote score: {this.props.voteScore}</p>
-        <div className="vote-widget__vote-btn-container">
-          <button 
-            className="btn vote-widget__vote-btn vote-widget__vote-btn--up"
-            onClick={() => this.props.dispatch(vote(this.props.id, 'upVote', this.props.type))}
-          >
-            <i className="fas fa-thumbs-up"></i>
-          </button>
-          <button
-            className="btn vote-widget__vote-btn vote-widget__vote-btn--down"
-            onClick={() => this.props.dispatch(vote(this.props.id, 'downVote', this.props.type))}
-          >
-            <i className="fas fa-thumbs-down"></i>
-          </button>
-        </div>
-      </div>
-    )
-  }
-}
+const VoteWidget = (props) => (
+<div className="vote-widget__container">
+  <p className="vote-widget__votes">vote score: {props.voteScore}</p>
+  <div className="vote-widget__vote-btn-container">
+    <button 
+      className="btn vote-widget__vote-btn vote-widget__vote-btn--up"
+      onClick={() => props.dispatch(vote(props.id, 'upVote', props.type))}
+    >
+      <i className="fas fa-thumbs-up"></i>
+    </button>
+    <button
+      className="btn vote-widget__vote-btn vote-widget__vote-btn--down"
+      onClick={() => props.dispatch(vote(props.id, 'downVote', props.type))}
+    >
+      <i className="fas fa-thumbs-down"></i>
+    </button>
+  </div>
+</div>
+)
 
-export default connect()(VoteWidget);
+export default connect(null, null)(VoteWidget);
