e28a96abaec98c548e731e4b6b7460f90812edb4
Arend Hosman <info@webbb-online.com>
2018-03-18 17:06:38 +0100

    Removes unnecessary dispatch

--- frontend/src/components/CategoryList/CategoryListContainer.js
+++ frontend/src/components/CategoryList/CategoryListContainer.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { getPostsInCategory } from '../../actions/postActions'
-
 import Container from '../Container'
 import LoadingIndicator from '../LoadingIndicator'
 import PostList from '../PostList/PostList'
 
 class CategoryListContainer extends Component {
-
-  componentDidMount() {
-    this.props.dispatch(getPostsInCategory(this.props.posts.items));
-  }
 
   renderPostsOrLoadingIndicator() {
     if(this.props.isFetching) {
